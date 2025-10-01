e8263fc1e5ca83ddd27b458b189552b740d45ff2
Nenavath Jeevan <nenavathjeevan2004@gmail.com>
2025-10-01 07:54:59 +0530

    Update LCA in BST.cpp

--- BST/LCA in BST.cpp
+++ BST/LCA in BST.cpp
@@ -40,8 +40,8 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL) return NULL;
         int curr = root->val;
-        if(curr < p->val && curr < q->val) return lowestCommonAncestor(root->right, p, q);
-        if(curr>p->val && curr>q->val) return lowestCommonAncestor(root->left,p,q);
+        if( curr < p->val && curr < q->val ) return lowestCommonAncestor(root->right, p, q);
+        if( curr > p->val && curr > q->val ) return lowestCommonAncestor(root->left, p, q);
 
         return root;
         
