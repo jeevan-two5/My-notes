2a5eb4cee5f2b1a3d308c47c87de3c2b6b2cb8fc
Nenavath Jeevan <nenavathjeevan2004@gmail.com>
2025-10-01 07:30:41 +0530

    Update KthSmallest element in bst.cpp

--- BST/KthSmallest element in bst.cpp
+++ BST/KthSmallest element in bst.cpp
@@ -12,3 +12,26 @@ Example 2:
 Input: root = [5,3,6,2,4,null,null,1], k = 3
 Output: 3
 */
+
+//          --------------------------O P T I M A L   S O L U T I O N -----------------------------
+
+
+int inorder(TreeNode*node,int &cnt ,int k){
+    if(node == NULL) return -1;
+    int left = inorder(node->left,cnt,k);
+    if(left!=-1) return left;
+    cnt++;
+    if(cnt ==k) return node->val;
+ 
+    return inorder(node->right,cnt,k);
+ }
+
+
+
+    int kthSmallest(TreeNode* root, int k) {
+        int cnt =0;
+       int ans= inorder(root,cnt,k);
+       return ans;
+    }
+
+// TC : O(N)  SC: O(1)
