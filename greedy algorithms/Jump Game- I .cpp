f303322ce7ad4cb7003103ca6d519108bfe4562d
Nenavath Jeevan <nenavathjeevan2004@gmail.com>
2025-12-14 04:24:39 +0530

    Implement Jump Game I solution
    Add function to determine if the last index can be reached.

--- greedy algorithms/Jump Game- I .cpp
+++ greedy algorithms/Jump Game- I .cpp
@@ -1,1 +1,11 @@
 
+bool f(vector<int>&arr){
+  int n = arr.size();
+  int goal = n - 1;
+  for(int i = n-2; i >= 0; --i){
+    if(i + arr[i] >= goal) goal = i;
+    
+  }
+  if(goal == 0) return true; 
+  return false;
+}
