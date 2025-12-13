92f91dc90eced7afa9cb1fd286a914de1e738776
Nenavath Jeevan <nenavathjeevan2004@gmail.com>
2025-12-14 05:08:25 +0530

    Update N meeting in one room.cpp

--- greedy algorithms/N meeting in one room.cpp
+++ greedy algorithms/N meeting in one room.cpp
@@ -1,1 +1,19 @@
+int max_meetings(int start[] , int end[] , int n){
+  vector<pair<int, int>> meet;
+  for(int i = 0 ; i < n; i++){
+    meet.push_back( {end[i]}  ,  {start[i]}  );
+  }
+  sort(meet.begin() , meet.end());
 
+  int ans = 1; prev = meet[0].first;
+
+  for(int i = 1 ; i < n; i++ ){
+    if(meet[i].second > prev) {
+      ans++;
+      prev = meet[i].first;
+    }
+  }
+
+  return ans;
+  
+}
