109cb8e20120c22fde2af52943ba758093312523
Nenavath Jeevan <nenavathjeevan2004@gmail.com>
2025-12-14 05:36:31 +0530

    Add function to calculate non-overlapping intervals
    Implement function to count non-overlapping intervals.

--- greedy algorithms/Non overlapping intervals.cpp
+++ greedy algorithms/Non overlapping intervals.cpp
@@ -1,1 +1,18 @@
 
+static bool comp(vector<int>&a, vector<int>&b){
+  return a[1] < b[1];
+}
+
+int f(vector<vector<int>>&I){ // I - intervals
+  int n = I.size();
+  sort(I.begin(), I.end());
+  int ans = 1;
+  int prev = I[0][1];
+  for(int i = 1 ; i < I.size() ; i++){
+    if(I[i][0] >= prev) {  // > , >= in both questions this and N meetings
+      prev = I[i][1];
+      ans++;
+    }
+  }
+  return (n - ans);
+}
