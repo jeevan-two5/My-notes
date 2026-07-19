5c83bd69db0eeefbfbf4dca469396e5d01af4542
Nenavath Jeevan <nenavathjeevan2004@gmail.com>
2026-07-19 14:24:18 +0530

    Update 17.cpp

--- 17.cpp
+++ 17.cpp
@@ -1,32 +1,20 @@
-#include <vector>
-#include <numeric>
-#include <bitset>
-#include <algorithm>
-
-using namespace std;
-
 int getMinProcessingTime(vector<int> data, int processTimeA, int processTimeB) {
-    int total_sum = 0;
-    for (int x : data) total_sum += x;
+    int total = 0;
+    for (int x : data) total += x;
     
-    // Max possible sum is 100 * 1000 = 100000
-    // Using bitset for ultra-fast subset sum DP
-    bitset<100005> reachable;
-    reachable[0] = 1;
+    bitset<100005> dp;
+    dp[0] = 1;
     
     for (int x : data) {
-        reachable |= (reachable << x);
+        dp |= (dp << x);
     }
     
-    int best_time = 2e9;
-    
-    for (int i = 0; i <= total_sum; ++i) {
-        if (reachable[i]) {
-            int timeA = i * processTimeA;
-            int timeB = (total_sum - i) * processTimeB;
-            best_time = min(best_time, max(timeA, timeB));
+    int ans = 2e9;
+    for (int i = 0; i <= total; ++i) {
+        if (dp[i]) {
+            int cost = max(i * processTimeA, (total - i) * processTimeB);
+            if (cost < ans) ans = cost;
         }
     }
-    
-    return best_time;
+    return ans;
 }
